3e31b1732bbcd51f71d4b15713eb7dff959c7576
bungafdl <bungaafdl@gmail.com>
2019-08-10 18:07:18 +0700

    first commit

---
+++ app/User.php
@@ -0,0 +1,31 @@
+<?php
+namespace App;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+class User extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+use Illuminate\Http\Request;
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function($api){
+    $api->group(['middleware' => 'auth.jwt'], function ($api) {
+        $api->DELETE('logout', 'App\Http\Controllers\AuthController@logout');
+        $api->GET('me', 'App\Http\Controllers\AuthController@show');
+    });
+    $api->POST('register', 'App\Http\Controllers\AuthController@register');
+    $api->POST('login', 'App\Http\Controllers\AuthController@login');
+});
